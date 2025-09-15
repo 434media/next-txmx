40f29b66aaba6199e3893b40a87b27032d756127
Jesse Hernandez <jesseovr@gmail.com>
2025-09-15 12:44:31 -0500

    mailchimp trigger tags added

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -89,7 +89,7 @@ export async function POST(request: Request) {
         {
           email_address: email,
           status: "subscribed",
-          tags: ["web-txmxboxing"],
+          tags: ["web-txmxboxing", "newsletter-signup"],
         },
         {
           auth: {
@@ -135,7 +135,7 @@ export async function POST(request: Request) {
             await axios.patch(
               `https://${mailchimpDatacenter}.api.mailchimp.com/3.0/lists/${mailchimpListId}/members/${emailHash}`,
               {
-                tags: ["web-txmxboxing"],
+                tags: ["web-txmxboxing", "newsletter-signup"],
               },
               {
                 auth: {
